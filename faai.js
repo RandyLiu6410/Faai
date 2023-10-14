0c2d6c470e385d2611db39ba625fa69a9d1d86b9
Randy Liu <wert6410@gmail.com>
2023-10-14 13:25:53 +0800

    feat: init http server

--- faai.js
+++ faai.js
@@ -1,1 +1,13 @@
-console.log("Hello via Bun!");
+import http from "http"
+import color from "picocolors"
+
+http
+  .createServer((req, res) => {
+    res.end("hi")
+  })
+  .listen(process.env.HTTP_PORT)
+
+console.log(
+`${color.bgBlue(process.env.PROJECT_NAME)} is running now
+${color.green(`http://127.0.0.1:${process.env.HTTP_PORT}`)}`
+)
