ae9e2e40a2f1ace3b16a4dff393f878b46a562a6
Randy Liu <wert6410@gmail.com>
2023-10-14 13:46:04 +0800

    feat: init ws server

--- faai/server.js
+++ faai/server.js
@@ -2,15 +2,31 @@ import http from "http"
 import color from "picocolors"
 import connect from "connect"
 import { indexHTMLMiddleware } from "./middleware"
+import { WebSocketServer } from "ws"
+
+const { PROJECT_NAME, HTTP_PORT, WS_PORT } = process.env
+
+const createWSServer = () => {
+  const server = new WebSocketServer({
+    port: WS_PORT,
+  })
+
+  server.on("connection", (ws) => {
+    console.log(color.bgCyan("WS server connected!"))
+
+    ws.on("message", (data) => {
+      console.log("Received: %s", data)
+    })
+  })
+}
 
 const middleware = connect()
-
-const { PROJECT_NAME, HTTP_PORT } = process.env
-
 middleware.use(indexHTMLMiddleware)
 
 export function createServer() {
   http.createServer(middleware).listen(HTTP_PORT)
+
+  createWSServer()
 
   console.log(
     `${color.bgBlue(PROJECT_NAME)} is running now
